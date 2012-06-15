24e7a0867ecd74198fd135c76d05cc07d209c0cc
Mr Rogers <jon@bunnymatic.com>
2012-06-14 23:50:08 -0700

    * faye pubsub server with enough ruby to get it on heroku

---
+++ maucomm.js
@@ -0,0 +1,17 @@
+var http = require('http'),
+  faye = require('faye');
+
+var bayeux = new faye.NodeAdapter({mount:'/maucomm'});
+
+port = process.env.PORT || 3030;
+
+var server = http.createServer(function(req,res) {
+  res.writeHead(200, {'content-type': 'text/plain'});
+  res.write('Tune in to ' + port + 'MHz for the best funk around');
+  res.end();
+});
+
+console.log("Listening on port " + port);
+bayeux.attach(server);
+
+server.listen(port);
