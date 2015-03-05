34eb271693d766acd6ef424e083f63d77d029201
David Furber <furberd@gmail.com>
2015-03-04 23:00:58 -0500

    major overhaul including ssl support

--- server.js
+++ server.js
@@ -1,19 +1,51 @@
-var http = require('http'),
-  faye = require('faye'),
+var faye = require('faye'),
   sha1 = require('sha1'),
   privatePub = require('./private_pub');
+
+if (!process.env.PRIVATE_PUB_SERVER)
+{
+  var yaml = require('js-yaml'),
+      fs   = require('fs'),
+      path = process.env.CONF || './.env';
+  
+  try {
+    var yamlFile = fs.readFileSync(path, 'utf8');
+    var config = yaml.safeLoad(yamlFile);
+    var keys = Object.keys(config);
+    for (i in keys) {
+      var key = keys[i];
+      process.env[key] = config[key];
+    }
+    console.log('Environment loaded.');
+  } catch (e) {
+    console.log(".env file not found");
+  }
+}
 
 var port = process.env.PORT || 3030;
 var bayeux = new faye.NodeAdapter({mount:'/faye'});
 bayeux.addExtension(privatePub);
 
-var server = http.createServer(function(req,res) {
+callback = function(req,res) {
   res.writeHead(200, {'content-type': 'text/plain', 'Access-Control-Allow-Origin': '*' });
   res.write('Tune in to ' + port + 'MHz for the best funk around');
   res.end();
-});
+}
 
-console.log("Listening on port " + port);
+if (process.env.PRIVATE_PUB_CERTIFICATE)
+{
+  var options = {}
+  options.key = fs.readFileSync(process.env.PRIVATE_PUB_PRIVATEKEY);
+  options.cert= fs.readFileSync(process.env.PRIVATE_PUB_CERTIFICATE);
+  console.log('Enabling SSL mode.');
+  var server = require('https').createServer(options, callback);
+} else {
+  var http = require('http');
+  var server = http.createServer(callback);
+}
+
 bayeux.attach(server);
 
-server.listen(port);
+server.listen(port, function(){
+  console.log("Listening on port " + port);
+});
